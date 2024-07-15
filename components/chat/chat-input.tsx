7bac2fdd7632bc3b0d9a6b6d5d59e30dbbfcb61c
sevensinjai <45555353+sevensinjai@users.noreply.github.com>
2024-07-15 17:29:08 +0800

    feat: hide file upload func

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -217,11 +217,11 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
         </div>
 
         <>
-          <IconCirclePlus
+          {/* <IconCirclePlus
             className="absolute bottom-[12px] left-3 cursor-pointer p-1 hover:opacity-50"
             size={32}
             onClick={() => fileInputRef.current?.click()}
-          />
+          /> */}
 
           {/* Hidden input to select files from device */}
           <Input
